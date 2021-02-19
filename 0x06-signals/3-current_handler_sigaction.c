aa2f25702ca6acfb2e8b93bae5548583645a9ec0
daorejuela1 <daorejuela1@outlook.com>
2021-02-19 05:30:50 +0000

    added task 3

---
+++ 0x06-signals/3-current_handler_sigaction.c
@@ -0,0 +1,15 @@
+#include "signals.h"
+
+/**
+ * handle_sigaction - set a handler for the signal SIGINT
+ *
+ * Return: retrieves the current handler of the signal SIGINT
+ */
+int handle_sigaction(void)
+{
+	struct sigaction act;
+
+	memset(&act, 0, sizeof(act));
+	sigaction(SIGINT, &act, NULL);
+	return (act.sa_handler);
+}
