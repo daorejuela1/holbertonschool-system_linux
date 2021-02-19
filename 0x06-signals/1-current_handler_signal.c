47c6e2f432444d43496185a700400be70e1d06fb
daorejuela1 <daorejuela1@outlook.com>
2021-02-19 05:22:48 +0000

    added task 1

---
+++ 0x06-signals/1-current_handler_signal.c
@@ -0,0 +1,12 @@
+#include "signals.h"
+
+/**
+ * current_handler_signal - retrieve the current handler of SIGINT
+ *
+ * Return: NULL on error, pointer to the current handler of SIGINT otherwise
+ */
+void (*current_handler_signal(void))(int)
+{
+	__sighandler_t sigint_handler = signal(SIGINT, SIG_IGN);
+	return (sigint_handler);
+}
