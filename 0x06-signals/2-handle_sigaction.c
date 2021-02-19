e10c1c5d92f66f529a0b191d9b23c45f0f572ec3
daorejuela1 <daorejuela1@outlook.com>
2021-02-19 05:27:50 +0000

    added task 2

---
+++ 0x06-signals/2-handle_sigaction.c
@@ -0,0 +1,24 @@
+#include "signals.h"
+
+/**
+ * print_gotcha - receives signal
+ * @signal: the signal number received
+ */
+void print_gotcha(int signal)
+{
+	printf("Gotcha! [%d]\n", signal);
+}
+
+/**
+ * handle_sigaction - set a handler for the signal SIGINT
+ *
+ * Return: Return 0 on success, or -1 on error
+ */
+int handle_sigaction(void)
+{
+	struct sigaction act;
+
+	memset(&act, 0, sizeof(act));
+	act.sa_handler = print_gotcha;
+	return (sigaction(SIGINT, &act, NULL));
+}
