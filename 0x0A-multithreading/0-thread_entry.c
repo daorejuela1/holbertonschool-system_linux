7812d8c94fab5c57cda380a54431a93f11671823
daorejuela1 <daorejuela1@outlook.com>
2021-05-13 03:43:24 +0000

    Added task 0

--- 0x0A-multithreading/0-thread_entry.c
+++ 0x0A-multithreading/0-thread_entry.c
@@ -1,1 +1,12 @@
-#!/usr/bin/env bash
+#include "multithreading.h"
+
+/**
+ * thread_entry - serves as entry point to new thread
+ * @arg: holds address of string that must be printed and followed by new line
+ * Return: void
+ */
+void *thread_entry(void *arg)
+{
+	printf("%s\n", (char *)arg);
+	pthread_exit(NULL);
+}
