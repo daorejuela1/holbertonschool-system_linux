a7e0521b2ae226a9f9c7ea54c2d24f0e5d4a8d69
daorejuela1 <daorejuela1@outlook.com>
2021-05-16 00:41:31 +0000

    Added second task

--- 0x0A-multithreading/multithreading.h
+++ 0x0A-multithreading/multithreading.h
@@ -5,6 +5,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
+#include <sys/sysinfo.h>
+#include "list.h"
+#include "pool_threading.h"
+
+#define NUM_THREADS get_nprocs()
 
 /**
  * struct pixel_s - RGB pixel
@@ -70,4 +75,11 @@ typedef struct blur_portion_s
 
 void *thread_entry(void *arg);
 int tprintf(char const *format, ...);
+void blur_portion(blur_portion_t const *portion);
+void blur_image(img_t *img_blur, img_t const *img, kernel_t const *kernel);
+int tprintf(char const *format, ...);
+list_t *prime_factors(char const *s);
+task_t *create_task(task_entry_t entry, void *param);
+void destroy_task(task_t *task);
+void *exec_tasks(list_t const *tasks);
 #endif
